bd03be6317fb225371ed83cce74bc2bbb41e590f
Denis <Barkas074@yandex.ru>
2020-11-11 23:39:49 +0500

    Создан главный экран программы.

--- Feelwords/FeelwordsConsoleUI.cs
+++ Feelwords/FeelwordsConsoleUI.cs
@@ -1,14 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Feelwords
 {
-	class FeelwordsConsoleUI
+	class FeelwordsConsoleUI : TheNamesOfTheItems
 	{
+
 		public void DrawingMenu()
 		{
-			
+			Console.SetWindowSize(Console.WindowWidth, 42);
+			Console.Clear();
+			string[][] menuOption = new string[][] { NAMEGAME, NEWGAME, RESUME, RATING, EXIT };
+			int centerX;
+			int centerY = 1;
+			for (int i = 0; i < menuOption.Length; i++)
+			{
+				for (int j = 0; j < menuOption[i].Length; j++)
+				{
+					centerX = (Console.WindowWidth / 2) - (menuOption[i][j].Length / 2);
+					Console.SetCursorPosition(centerX, centerY);
+					Console.WriteLine(menuOption[i][j]);
+					centerY = Console.CursorTop;
+				}
+			}
+			Console.ReadKey();
+
 		}
 	}
 }
