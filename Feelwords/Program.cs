19d90e036b35920a983d1f4fc0ce463bf8ca7fc2
Denis <Barkas074@yandex.ru>
2020-11-12 20:27:50 +0500

    Добавлена заглушка на пункты меню.

--- Feelwords/Program.cs
+++ Feelwords/Program.cs
@@ -7,8 +7,15 @@ namespace Feelwords
 		static void Main(string[] args)
 		{
 			FeelwordsConsoleUI consoleUI = new FeelwordsConsoleUI();
-			consoleUI.Main();
-			
+			int choice = consoleUI.Main();
+			if (choice == 1)
+				consoleUI.DrawingNewGame();
+			else if (choice == 2)
+				consoleUI.DrawingResume();
+			else if (choice == 3)
+				consoleUI.DrawingRating();
+			else if (choice == 4)
+				Environment.Exit(0);
 		}
 	}
 }
